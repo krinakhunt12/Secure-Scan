b85192dd24033a18094ff708a4cdeef486b125cd
Krina Khunt <krina@codesmiths.in>
2026-04-21 14:14:16 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -778,4 +778,4 @@ def domain_info(domain: str):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
